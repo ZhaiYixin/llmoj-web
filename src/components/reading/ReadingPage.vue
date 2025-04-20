ef8810e29afc5be06d9e1d4e3b93147b94426c74
Zhai-yixin <17722941302@qq.com>
2025-04-20 16:20:44 +0800

    add: PDF工具栏

--- src/components/reading/ReadingPage.vue
+++ src/components/reading/ReadingPage.vue
@@ -1,11 +1,17 @@
 <template>
-  <ReadingPDFRender ref="pdfRenderRef" />
+  <div class="pdf-viewer">
+    <ReadingTopToolbar :num-pages="numPages" :current="current" v-model:scale="scale" v-model:rotation="rotation"
+      @jump="handleToolbarJump" @scale-fit="handleToolbarScaleFit" />
+    <ReadingPDFRender ref="pdfRenderRef" v-model:num-pages="numPages" v-model:current="current" v-model:scale="scale"
+      v-model:rotation="rotation" class="pdf-render" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 
 import { axiosInstance } from '@/services/http';
+import ReadingTopToolbar from '@/components/reading/ReadingTopToolbar.vue';
 import ReadingPDFRender from '@/components/reading/ReadingPDFRender.vue';
 
 const props = defineProps<{
@@ -13,6 +19,18 @@ const props = defineProps<{
 }>();
 
 const pdfRenderRef = ref();
+const numPages = ref(1);
+const current = ref(1);
+const scale = ref(1);
+const rotation = ref(0);
+
+const handleToolbarJump = (pageNum: number) => {
+  pdfRenderRef.value?.jumpToPage(pageNum);
+}
+
+const handleToolbarScaleFit = (mode: 'width' | 'height') => {
+  pdfRenderRef.value?.scaleFit(mode);
+}
 
 const loadPDFFile = async (pdf_id: string) => {
   const url = `/pdf/files/${pdf_id}/`;
@@ -29,4 +47,14 @@ watch(() => props.pdfId, () => {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.pdf-viewer {
+  display: flex;
+  flex-direction: column;
+}
+
+.pdf-render {
+  flex: 1;
+  background-color: #E6E8EB;
+}
+</style>
---
+++ src/components/reading/ReadingTopToolbar.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="toolbar">
+    <el-input-number v-model="scaleInPercent" @change="onScaleInPercentChange" :min="0" :step="20">
+      <template #decrease-icon>
+        <el-icon>
+          <Minus />
+        </el-icon>
+      </template>
+      <template #suffix>
+        <span>%</span>
+      </template>
+      <template #increase-icon>
+        <el-icon>
+          <Plus />
+        </el-icon>
+      </template>
+    </el-input-number>
+    <el-button @click="handleScaleFit">
+      <el-icon>
+        <FitWidthIcon v-if="scaleFitMode == 'width'" />
+        <FitHeightIcon v-else />
+      </el-icon>
+    </el-button>
+    <el-input-number v-model="currentPage" @change="onPageChange" :min="1" :max="numPages" step-strictly>
+      <template #decrease-icon>
+        <el-icon>
+          <ArrowLeft />
+        </el-icon>
+      </template>
+      <template #suffix>
+        <span>/ {{ props.numPages }}</span>
+      </template>
+      <template #increase-icon>
+        <el-icon>
+          <ArrowRight />
+        </el-icon>
+      </template>
+    </el-input-number>
+    <el-button @click="handleRotateRight">
+      <el-icon>
+        <RotateRightIcon />
+      </el-icon>
+    </el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { ElMessage } from 'element-plus';
+import { ArrowLeft, ArrowRight, Minus, Plus } from '@element-plus/icons-vue';
+import FitWidthIcon from '@/components/icons/IconFitWidth.vue'
+import FitHeightIcon from '@/components/icons/IconFitHeight.vue'
+import RotateRightIcon from '@/components/icons/IconRotateRight.vue'
+
+const props = defineProps<{
+  numPages: number;
+  current: number;
+}>();
+
+const emit = defineEmits<{
+  (event: 'jump', pageNum: number): void;
+  (event: 'scale-fit', mode: 'width' | 'height'): void;
+}>();
+
+const scale = defineModel<number>('scale', { default: 1 });
+const rotation = defineModel<number>('rotation', { default: 0 });
+
+const currentPage = ref(props.current);
+const scaleInPercent = ref(100);
+const scaleFitMode = ref<'width' | 'height'>('width');
+
+const onPageChange = (page: number) => {
+  if (page < 1 || page > (props.numPages || 1)) {
+    ElMessage.error('页码超出范围');
+    return;
+  }
+  emit('jump', page);
+};
+
+const onScaleInPercentChange = (scaleInPercentValue: number) => {
+  scale.value = scaleInPercentValue / 100;
+};
+
+const handleScaleFit = () => {
+  if (scaleFitMode.value == 'width') {
+    emit('scale-fit', 'width');
+    scaleFitMode.value = 'height';
+  } else {
+    emit('scale-fit', 'height');
+    scaleFitMode.value = 'width';
+  }
+}
+
+const handleRotateRight = () => {
+  rotation.value = (rotation.value + 90) % 360;
+};
+
+watch(() => props.current, () => {
+  currentPage.value = props.current;
+});
+
+watch(scale, (value) => {
+  scaleInPercent.value = Math.floor(100 * value);
+});
+</script>
+
+<style scoped>
+.toolbar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+  background-color: #FAFAFA;
+  border-bottom: var(--el-border);
+}
+</style>
